7c0e4b10e5ba2016bf9e2a136caabf9a49322da2
pganguru <pganguru@IN-5CG0251NSY>
2020-09-19 21:57:35 +0530

    StudentWebApp using springboot

---
+++ StudentWebAppSpring-1/src/main/java/com/dxc/StudentWebAppSpring1Application.java
@@ -0,0 +1,13 @@
+package com.dxc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudentWebAppSpring1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudentWebAppSpring1Application.class, args);
+	}
+
+}
